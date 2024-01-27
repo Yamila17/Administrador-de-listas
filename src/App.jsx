e7fece5d7a31ddd0e3c07657e82ade48a6a76704
Yamila <yamilarobles10@gmail.com>
2024-01-27 11:01:48 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Button } from 'react-bootstrap'
+
+function App() {
+ 
+
+  return (
+    <header>
+        <h1>Hola soy bootstrap en tu proyecto</h1>
+        <Button variant="warning">Warning</Button>
+    </header>
+  
+  )
+}
+
+export default App
